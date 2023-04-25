004e76a057054aa5fa849d63b034541f62d026d7
davincidreams <lisamegawatts@gmail.com>
2023-04-25 01:57:32 -0400

    Site Title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Lisa Mega Watts Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Lisa Mega Watts `,
+      summary: `who lives and works in Philadelphia building useful things.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog full of various things of interest.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `lisa_megawatts`,
     },
   },
   plugins: [
